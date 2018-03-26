8abc1d4335bffe61f9ed088daf09ff790373e0e9
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2018-03-26 16:36:50 +0530

    Update next3values20.cpp

--- next3values20.cpp
+++ next3values20.cpp
@@ -1,2 +1,18 @@
-abc
-def
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[30];
+    int i,n;
+    cout<<"enter n value:";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>str[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        str[i]=str[i]+3;
+        cout<<str[i];
+    }return 0;
+}
